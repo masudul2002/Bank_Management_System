db7f34611ad9603813c5dc84db21f5af20e2fb14
MASUDUL HASAN <masudul.sstu@gmail.com>
2025-11-17 18:02:00 +0600

    Update README.md

--- bank_system.cpp
+++ bank_system.cpp
@@ -1,92 +1,91 @@
-
 #include <iostream>
-#include <fstream>
-#include <string>
 using namespace std;
 
-class Bank {
+class BankAccount {
+private:
+    int accountNumber;
+    string accountHolderName;
+    double balance;
 public:
-    void createAccount();
-    void deposit();
-    void withdraw();
-    void balance();
+    void accountDetails() {
+        cout << "Enter Your Name: ";
+        cin.ignore();
+        getline(cin, accountHolderName);
+        cout << "Enter your Account Number: ";
+        cin >> accountNumber;
+        cout << "Enter your Initial Balance: ";
+        cin >> balance;
+    }
+
+    void Deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            cout << "Deposit Money: " << amount << endl;
+        } else {
+            cout << "Invalid amount" << endl;
+        }
+    }
+
+    void Withdraw(double amount) {
+        if (amount > 0 && balance >= amount) {
+            balance -= amount;
+            cout << "Withdraw amount: " << amount << endl;
+        } else if (amount > 0 && balance < amount) {
+            cout << "Insufficient balance" << endl;
+        } else {
+            cout << "Invalid amount" << endl;
+        }
+    }
+
+    void DisplayAccount() {
+        cout << "Account Holder Name: " << accountHolderName << endl;
+        cout << "Account Number: " << accountNumber << endl;
+        cout << "Balance: " << balance << endl;
+    }
+
+    // Friend function for transferring funds
+    friend void Transfer(BankAccount& from, BankAccount& to, double amount);
 };
 
-void Bank::createAccount(){
-    string name; int id; float bal;
-    cout<<"Enter Name: "; cin>>name;
-    cout<<"Enter ID: "; cin>>id;
-    cout<<"Initial Deposit: "; cin>>bal;
-    ofstream file("accounts.txt", ios::app);
-    file<<id<<" "<<name<<" "<<bal<<"\n";
-    cout<<"Account Created Successfully!\n";
+void Transfer(BankAccount& from, BankAccount& to, double amount) {
+    if (amount > 0 && from.balance >= amount) {
+        from.balance -= amount;
+        to.balance += amount;
+        cout << "Transferred " << amount << " from " << from.accountHolderName
+             << " to " << to.accountHolderName << endl;
+    } else if (amount > 0 && from.balance < amount) {
+        cout << "Insufficient balance for transfer" << endl;
+    } else {
+        cout << "Invalid transfer amount" << endl;
+    }
 }
 
-void Bank::deposit(){
-    int id; float amount;
-    cout<<"Enter ID: "; cin>>id;
-    cout<<"Deposit Amount: "; cin>>amount;
+int main() {
+    BankAccount account1, account2;
+    double amount;
 
-    ifstream in("accounts.txt");
-    ofstream out("temp.txt");
-    int fid; string name; float bal;
+    cout << "Enter details for Account 1:" << endl;
+    account1.accountDetails();
+    cout << "Enter details for Account 2:" << endl;
+    account2.accountDetails();
 
-    while(in>>fid>>name>>bal){
-        if(fid==id) bal+=amount;
-        out<<fid<<" "<<name<<" "<<bal<<"\n";
-    }
-    in.close(); out.close();
-    remove("accounts.txt"); rename("temp.txt","accounts.txt");
-    cout<<"Deposit Successful!\n";
-}
+    cout << "Enter amount to deposit in Account 1: ";
+    cin >> amount;
+    account1.Deposit(amount);
 
-void Bank::withdraw(){
-    int id; float amount;
-    cout<<"Enter ID: "; cin>>id;
-    cout<<"Withdraw Amount: "; cin>>amount;
+    cout << "Enter amount to withdraw from Account 1: ";
+    cin >> amount;
+    account1.Withdraw(amount);
 
-    ifstream in("accounts.txt");
-    ofstream out("temp.txt");
-    int fid; string name; float bal;
+    cout << "Enter amount to transfer from Account 1 to Account 2: ";
+    cin >> amount;
+    Transfer(account1, account2, amount);
 
-    while(in>>fid>>name>>bal){
-        if(fid==id){
-            if(bal>=amount) bal-=amount;
-            else cout<<"Insufficient Balance!\n";
-        }
-        out<<fid<<" "<<name<<" "<<bal<<"\n";
-    }
-    in.close(); out.close();
-    remove("accounts.txt"); rename("temp.txt","accounts.txt");
-    cout<<"Withdrawal Completed!\n";
-}
+    cout << "\nAfter Transactions:" << endl;
+    cout << "Account 1:" << endl;
+    account1.DisplayAccount();
+    cout << "Account 2:" << endl;
+    account2.DisplayAccount();
 
-void Bank::balance(){
-    int id;
-    cout<<"Enter ID: "; cin>>id;
-
-    ifstream in("accounts.txt");
-    int fid; string name; float bal;
-
-    while(in>>fid>>name>>bal){
-        if(fid==id){
-            cout<<"Name: "<<name<<" | Balance: "<<bal<<"\n";
-            return;
-        }
-    }
-    cout<<"Account Not Found!\n";
-}
-
-int main(){
-    Bank b;
-    int choice;
-    while(true){
-        cout<<"\n1.Create 2.Deposit 3.Withdraw 4.Balance 5.Exit\nEnter: ";
-        cin>>choice;
-        if(choice==1) b.createAccount();
-        else if(choice==2) b.deposit();
-        else if(choice==3) b.withdraw();
-        else if(choice==4) b.balance();
-        else break;
-    }
+    return 0;
 }
